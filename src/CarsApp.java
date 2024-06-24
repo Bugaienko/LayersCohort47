a9738bd6dbaceb2b0aa9f7bf44cfb8b84265992c
SergeyBugaenko <sergiibugaienko@gmail.com>
2024-06-24 21:29:18 +0200

    Initial commit

---
+++ src/CarsApp.java
@@ -0,0 +1,21 @@
+/*
+@date 24.06.2024
+@author Sergey Bugaienko
+*/
+
+import repository.CarRepository;
+import repository.UserRepository;
+import service.CarService;
+import view.Menu;
+
+public class CarsApp {
+    public static void main(String[] args) {
+        UserRepository userRepository = new UserRepository();
+        CarRepository carRepository = new CarRepository();
+
+        CarService service = new CarService(carRepository, userRepository);
+        Menu menu = new Menu(service);
+
+        menu.run();
+    }
+}
